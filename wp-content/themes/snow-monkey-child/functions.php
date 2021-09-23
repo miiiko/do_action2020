4c4ad6ea5225185d3e6d7279f777d582f5b34b44
sainome <ymk@sainome.fun>
2021-09-23 13:21:15 +0900

    FullCalendar 追加+α

--- wp-content/themes/snow-monkey-child/functions.php
+++ wp-content/themes/snow-monkey-child/functions.php
@@ -5,6 +5,9 @@
 			get_stylesheet_uri(),
 			[ get_template() ]
 		);
+		wp_enqueue_style( 'fullcalendar-style', get_stylesheet_directory_uri() . '/fullcalendar/main.min.css');
+		wp_enqueue_script( 'fullcalendar-main-script', get_stylesheet_directory_uri() . '/fullcalendar/main.min.js', array(), '5.9.0', true );
+		wp_enqueue_script( 'fullcalendar-script', get_stylesheet_directory_uri() . '/fullcalendar/calendar.js', array(), '1.0.0', true );
 	} );
 	function post_has_archive($args, $post_type){
 		if('post' == $post_type){
